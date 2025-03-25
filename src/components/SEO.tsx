d786e0d9ee6be1e36353b0853a61f37ffe054301
Lucas Amaral <lucasahenriqueamaral@gmail.com>
2025-03-25 06:28:36 -0400

    adiciona favicons

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -43,11 +43,28 @@ export default function SEO({
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={ogImage} />
 
-      {/* Canonical URL */}
       <link rel="canonical" href={canonicalUrl} />
 
-      {/* Favicon */}
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 }
